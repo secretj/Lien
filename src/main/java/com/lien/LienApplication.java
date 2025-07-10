b91aecec7ad0f0b524d7585c1daf3e2839b3bc71
SecretJ <qkrwlsgud890@gmail.com>
2025-07-10 14:31:02 +0900

    init : Lien init

---
+++ src/main/java/com/lien/LienApplication.java
@@ -0,0 +1,13 @@
+package com.lien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LienApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LienApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lien/LienApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lien;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LienApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
